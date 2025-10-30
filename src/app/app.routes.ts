c5432905705f176462cb32c1ad9307024a3dc73a
JuamchoOrion <juand.ramirezc@uqvirtual.edu.co>
2025-10-30 14:16:04 -0500

    Agrega nuevas páginas y actualiza configuración de Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Recover } from './pages/recover/recover';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'recover', component: Recover },
+  { path: '**', redirectTo: 'login' },
+];
