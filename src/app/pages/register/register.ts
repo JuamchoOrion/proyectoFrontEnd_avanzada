f68cce5b473c262ac93dedb21cc2b6498f36cc8b
JuamchoOrion <juand.ramirezc@uqvirtual.edu.co>
2025-11-08 22:22:31 -0500

    register Service

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.services';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrls: ['./register.css'],
 })
-export class Register {}
+export class Register {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      role: ['GUEST', [Validators.required]],
+      dateBirth: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Datos enviados:', this.registerForm.value);
+
+    this.authService.register(this.registerForm.value).subscribe({
+      next: (response) => {
+        console.log('✅ Registro exitoso', response);
+        alert('Registro exitoso');
+      },
+      error: (error) => {
+        console.error('❌ Error en el registro', error);
+        alert('Error en el registro');
+      },
+    });
+  }
+}
---
+++ src/app/services/user.services.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class User {
+  private apiUrl = 'http://localhost:8080/api/users';
+}
