118cfd313ae6684af14ce7a72ef75aec11d03916
JuamchoOrion <juand.ramirezc@uqvirtual.edu.co>
2025-11-08 22:59:32 -0500

    login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.services';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css'],
 })
-export class Login {}
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log('Datos enviados:', this.loginForm.value);
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('✅ Login exitoso', response);
+        alert('Login exitoso');
+      },
+      error: (error) => {
+        console.error('❌ Error en el login', error);
+        alert('Error en el login');
+      },
+    });
+  }
+}
