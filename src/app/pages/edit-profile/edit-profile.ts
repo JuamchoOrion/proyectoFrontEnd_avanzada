93d089a6a621d10a6b0a881c2a8901979b7729af
Laura Valentina Gomez <lauritagomezalzate@gmail.com>
2025-11-12 21:49:22 -0500

    resolvi conflictos

--- src/app/pages/edit-profile/edit-profile.ts
+++ src/app/pages/edit-profile/edit-profile.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditProfileFormComponent } from '../../components/edit-profile-form/edit-profile-form';
+import { SidebarProfileComponent } from '../../components/sidebar-profile/sidebar-profile';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, SidebarProfileComponent, EditProfileFormComponent],
   templateUrl: './edit-profile.html',
-  styleUrl: './edit-profile.css',
+  styleUrls: ['./edit-profile.css'],
 })
-export class EditProfile {
-
-}
+export class EditProfilePage {}
